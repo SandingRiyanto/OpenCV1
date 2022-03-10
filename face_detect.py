9820f88d970207770e7f167e54678c002e6396a0
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-10 10:22:47 +0700

    diubah bagian nama folder images

--- face_detect.py
+++ face_detect.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+# diambil dari opencv github
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
@@ -16,6 +17,6 @@ for (x,y,w,h) in faces:
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-cv2.imshow('img',img)
+cv2.imshow('Display Images',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
