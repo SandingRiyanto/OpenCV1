eb998a28428aec74f197ab49cc621bce2e8ed235
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-10 10:20:32 +0700

    perubahan di nama folder images

--- display1.py
+++ display1.py
@@ -5,6 +5,6 @@ ret, biner = cv2.threshold(gray, 95, 255, cv2.THRESH_BINARY)
 
 # cv2.imshow('gambar madam lena', img)
 # cv2.imshow('gambar madam lena GRAY', gray)
-cv2.imshow('gambar madam lena BINER', biner)
+cv2.imshow('Citra Biner', biner)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
