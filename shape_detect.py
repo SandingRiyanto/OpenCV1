aa92142fe2938be0ff9dd7a38c47f4a48013201f
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-14 08:57:41 +0700

    testing

--- shape_detect.py
+++ shape_detect.py
@@ -1,42 +1,67 @@
-from cv2 import cv2
+import cv2
 import numpy as np
-from decimal import Decimal, getcontext
+from matplotlib import pyplot as plt
 
-# rescale frame (bikin default 75)
-def rescale_frame(frame, percent=75):
-    width   =int(frame.shape[1]*percent/100)
-    height  =int(frame.shape[0]*percent/100)
-    dim     =(width,height)
-    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+# reading image
+img = cv2.imread('shape_color.jpg')
 
-# aktifkan webcam
-cap = cv2.VideoCapture(0)
-while(True):
-    _,frame = cap.read()
-    image   = rescale_frame(frame, percent=100)
-    abu     = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    tepi    = cv2.Canny(abu, 100, 200)
-    kontur, hirarki = cv2.findContours(tepi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+# converting image into grayscale image
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# setting threshold of gray image
+_, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-    # hitung jumlah
-    jumlah = str(len(kontur))
-    print("Jumlah: ", jumlah)
+# using a findContours() function
+contours, _ = cv2.findContours(
+	threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    hasil_kontur = cv2.drawContours(image, kontur, -1, (0,255,0), 2)
+i = 0
 
-    for i in range(int(jumlah)):
-        area = cv2.contourArea(kontur[i])
-        perimeter = cv2.arcLength(kontur[i], True)
-        TR = (4*np.pi*area)/(perimeter**2)
+# list for storing names of shapes
+for contour in contours:
 
-        print(TR)
+	# here we are ignoring first counter because
+	# findcontour function detects whole image as shape
+	if i == 0:
+		i = 1
+		continue
 
-    cv2.imshow("hasil kontur", hasil_kontur)
-    cv2.imshow("Canny", tepi)
+	# cv2.approxPloyDP() function to approximate the shape
+	approx = cv2.approxPolyDP(
+		contour, 0.01 * cv2.arcLength(contour, True), True)
+	
+	# using drawContours() function
+	cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
 
-    interupsi = cv2.waitKey(10)
-    if interupsi & 0xFF == 27:
-        break
+	# finding center point of shape
+	M = cv2.moments(contour)
+	if M['m00'] != 0.0:
+		x = int(M['m10']/M['m00'])
+		y = int(M['m01']/M['m00'])
+
+	# putting shape name at center of each shape
+	if len(approx) == 3:
+		cv2.putText(img, 'Triangle', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	elif len(approx) == 4:
+		cv2.putText(img, 'Quadrilateral', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	elif len(approx) == 5:
+		cv2.putText(img, 'Pentagon', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	elif len(approx) == 6:
+		cv2.putText(img, 'Hexagon', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	else:
+		cv2.putText(img, 'circle', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+# displaying the image after drawing contours
+cv2.imshow('shapes', img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
