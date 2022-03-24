5990ca3aed46e818a732099b99420e318e897bd4
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-24 19:56:20 +0700

    current update

--- face_detect_img.py
+++ face_detect_img.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 
 # diambil dari opencv github
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('opencv_xml/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('opencv_xml/haarcascade_eye.xml')
 
 img = cv2.imread('images/woro.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
