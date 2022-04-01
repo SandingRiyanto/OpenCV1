21191cc21c38b11ea7120057b40ca2e851af767d
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-04-01 08:13:50 +0700

    current update

--- edge_detect_img.py
+++ edge_detect_img.py
@@ -2,7 +2,7 @@ from cv2 import cv2
 import numpy as np
 
 # akuisisi citra
-image = cv2.imread('images/bentuk.jpg')
+image = cv2.imread('images/shape_color.jpg')
 abu     = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 tepi    = cv2.Canny(abu, 100, 200)
 kontur, hirarki = cv2.findContours(tepi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -15,5 +15,7 @@ hasil_kontur = cv2.drawContours(image, kontur, -1, (0,255,0), 2) #format warna: 
 
 # cv2.imshow("hasil kontur", hasil_kontur)
 cv2.imshow("Canny", tepi)
+cv2.imwrite('images/saved.jpg', tepi)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ saved_loop.py
@@ -0,0 +1,17 @@
+# import the library opencv
+import cv2
+# globbing utility.
+import glob
+# select the path
+path = "images\*.*"
+for bb,file in enumerate (glob.glob(path)):
+    image_read = cv2.imread(file)
+    # conversion numpy array into rgb image to show
+    c = cv2.cvtColor(image_read, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Color image', c)
+    # writing the images in a folder output_images
+    cv2.imwrite('images\simpan\messigray{}.png'.format(bb), c)
+    # wait for 1 second
+    k = cv2.waitKey(1000)
+    # destroy the window
+    cv2.destroyAllWindows()
