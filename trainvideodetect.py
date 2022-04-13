7839a52dae26d04e23b774ca0a562ac1fb4b2c1a
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-04-13 13:20:52 +0700

    current update

---
+++ trainvideodetect.py
@@ -0,0 +1,8 @@
+# import required package
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+
+# initialize the image data generator with rescaling
+train_data_gen = Image
